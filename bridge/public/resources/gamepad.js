348cc426d7969ab1ffdb2a0dc3fd33749e2edc54
Adam Howard <adz@skatty.me>
2013-01-12 22:54:08 +0000

    FIX YES!

--- bridge/public/resources/gamepad.js
+++ bridge/public/resources/gamepad.js
@@ -23,6 +23,7 @@ $(function(){
 
   // Generic controller update check
   function updateController() {
+    // console.log('updating');
     // console.log(controller && controller.state && controller.state.x);
    // Clear button pressed styles
     var buttonsDom = document.getElementsByClassName("button");
@@ -39,26 +40,28 @@ $(function(){
    if (controller) {
 // console.log('hi');
      // Buttons
+     // console.log(controller.state);
      for (var buttonName in controller.state) {
        var button = controller.state[buttonName];
        var buttonType = layout[buttonName].type;
-       
-       if (!button) return
+       // console.log(button);
+       if (button) {
   
-       if (buttonType == 'button') {
-         // console.log(buttonDivs);
-         // console.log(buttonName);
-         // console.log(buttonDivs[buttonName]);
-         buttonDivs[buttonName].classList.add('pressed');
-       } else if (buttonType == 'dpad') {
-         console.log('is dpad');
-         if (button.up) xBoxDpadUp.classList.add('pressed');
-         if (button.down) xBoxDpadDown.classList.add('pressed');
-         if (button.left) xBoxDpadLeft.classList.add('pressed');
-         if (button.right) xBoxDpadRight.classList.add('pressed');
-       } else if (buttonType == 'joy') {
-         xBoxLeftStick.getElementsByTagName("span")[0].style.left = (button.x*20).toString()+"px";
-         xBoxLeftStick.getElementsByTagName("span")[0].style.top = (button.y*20).toString()+"px";
+         if (buttonType == 'button') {
+           // console.log(buttonDivs);
+           // console.log(buttonName);
+           // console.log(buttonDivs[buttonName]);
+           buttonDivs[buttonName].classList.add('pressed');
+         } else if (buttonType == 'dpad') {
+           console.log('is dpad');
+           if (button.up) xBoxDpadUp.classList.add('pressed');
+           if (button.down) xBoxDpadDown.classList.add('pressed');
+           if (button.left) xBoxDpadLeft.classList.add('pressed');
+           if (button.right) xBoxDpadRight.classList.add('pressed');
+         } else if (buttonType == 'joy') {
+           xBoxLeftStick.getElementsByTagName("span")[0].style.left = (button.x*20).toString()+"px";
+           xBoxLeftStick.getElementsByTagName("span")[0].style.top = (button.y*20).toString()+"px";
+         }
        }
      }
 
